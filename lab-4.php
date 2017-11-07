6e6cebbc0834929ee5b8785413edfd180ef3d296
eshabalin <p0kku@yandex.ru>
2017-11-07 10:41:45 +0300

    Update lab-4.php

--- lab-4.php
+++ lab-4.php
@@ -1,15 +1,15 @@
 <?php
 
-//$roof = array(2, 5, 1, 2, 3, 7, 7, 6); //òðåáóåìûé íàáîð öèôð
-for ($i = 0; $i < 9; $i++)	{$roof[] = rand(1, 9);} // ñëó÷àéíûé íàáîð öèôð
+//$roof = array(2, 5, 1, 2, 3, 7, 7, 6); 
+for ($i = 0; $i < 9; $i++)	{$roof[] = rand(1, 9);} 
 
 $kolvo = count($roof);
 for ($i = 0; $i < $kolvo; $i++)
 {
 print "$roof[$i] ";
 }
-$left = 0; //ïåðâûé  ýëåìåíò êðûøè
-$right = $kol-1; // ïîñëåäíèé ýëåìåíò êðûøè
+$left = 0; 
+$right = $kol-1;
 $max = max($roof);
 $leftmax = $roof[$left];
 $rightmax = $roof[$right];
